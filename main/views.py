03dcb7591bfabe6cd9090a38ce8a7ad0390d8310
Daniel Havlíček <daniel.havlicek.s@gyarab.cz>
2026-04-15 12:45:49 +0200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Submission
 
 def home(request):
     return render(request, "home.html")
 
-def challenge(request):
-    return render(request, "challenge.html")
 
 def leaderboard(request):
     return render(request, "leaderboard.html")
+
+
+def challenge(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        reps = request.POST.get('reps')
+        video_link = request.POST.get('video_link')
+
+        Submission.objects.create(
+            name=name,
+            reps=reps,
+            video_link=video_link
+        )
+
+        return redirect('leaderboard')
+
+    return render(request, 'challenge.html')
