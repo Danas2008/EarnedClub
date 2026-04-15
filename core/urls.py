075c882666a96ff67a9163918a9a0e24b6d0441f
Daniel Havlíček <daniel.havlicek.s@gyarab.cz>
2026-04-15 12:47:25 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
+from main import views
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+path('challenge/', views.challenge, name='challenge'),
 ]
