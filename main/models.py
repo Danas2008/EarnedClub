e32c36e47057ea762a7e19771407d0e9df9fdeec
Daniel Havlíček <daniel.havlicek.s@gyarab.cz>
2026-04-15 12:43:23 +0200

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
 class Submission(models.Model):
     name = models.CharField(max_length=100)
-    pushups = models.IntegerField()
+    reps = models.IntegerField()
     video_link = models.URLField()
-    verified = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} - {self.pushups}"
+        return f"{self.name} - {self.reps}"
